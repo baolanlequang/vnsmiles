82e2b7f94574406ede54a805b1cf539a4324f592
Bao Lan Le Quang <baolan2005@gmail.com>
2021-03-05 20:49:54 +0100

    init project

---
+++ VNSmiles/VNSmiles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VNSmiles
+//
+//  Created by Bao Lan Le Quang on 05/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
